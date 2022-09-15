3d51e4e509e2823a2faa9e0e7df615f6354a9c2e
William <wtladerer@gmail.com>
2022-09-14 18:53:35 -0700

    rearranging code for ease of reading

--- core/bulk2slab.py
+++ core/bulk2slab.py
@@ -1,34 +1,37 @@
-import pymatgen.core
-from pymatgen.core.composition import Element, Composition
-from pymatgen.core.periodic_table import Specie
 from pymatgen.core import Lattice, Structure, Molecule
 from pymatgen.core.surface import SlabGenerator
 from pymatgen.io.vasp import Poscar, Kpoints, Incar
-from pymatgen.io.cif import CifWriter
+from pymatgen.symmetry.bandstructure import HighSymmKpath
 from pymatgen.analysis.adsorption import plot_slab
 from pymatgen.analysis.adsorption import AdsorbateSiteFinder
 from matplotlib import pyplot as plt
+import functools
+
 
 class Bulk:
     
-    def __init__(self, CONTCAR):
+    def __init__(self, CONTCAR: str):
             self.CONTCAR = CONTCAR
-            self.structure = Structure.from_file(CONTCAR)
-            self.formula = ''.join(self.structure.formula.split())
+            self.structure: Structure = Structure.from_file(CONTCAR)
+            self.formula: str = ''.join(self.structure.formula.split())
 
 
 class Slab(Bulk):
 
-    def __init__(self, valid_slab, CONTCAR, index, MIN_SLAB, MIN_VAC):
+    def __init__(self, valid_slab, CONTCAR: str, index: tuple, MIN_SLAB: int, MIN_VAC: int):
         super().__init__(CONTCAR)
-        self.structure = valid_slab
-        self.length = valid_slab.as_dict()['lattice']['c']
-        self.filename = f"{self.formula}_{str(index[0]) + str(index[1]) + str(index[2])}_POSCAR.vasp"
+        self.structure: Structure = valid_slab
+        self.length: float = valid_slab.as_dict()['lattice']['c']
+        self.a: float = valid_slab.as_dict()['lattice']['a']
+        self.b: float = valid_slab.as_dict()['lattice']['b']
+        self.c: float = valid_slab.as_dict()['lattice']['c']
+        self.plane = f"{str(index[0]) + str(index[1]) + str(index[2])}"
+        self.filename: str = f"{self.formula}_{self.plane}_POSCAR.vasp"
     
     def __str__(self):
         return str(self.structure)
 
-    def save_slab(self):
+    def saveSlab(self):
 
         POSCAR = Poscar(self.structure, sort_structure=True)
         POSCAR.write_file(self.filename)
@@ -52,12 +55,74 @@ class Slab(Bulk):
                 ax.set_ylim(-5,5)
             plt.show()
 
-        return ads_struct
+        return ads_structs
 
-def find_slabs(bulk, index, MIN_SLAB, MIN_VAC):
-        slabgen = SlabGenerator(bulk.structure, index, MIN_SLAB, MIN_VAC, primitive=False, in_unit_planes=True) #this will find all unique surfaces of the structure within the specified miller plane
-        slabs = slabgen.get_slabs()
-        valid_slabs = [slab for slab in slabs if (not slab.is_polar() and slab.is_symmetric())] #list comprehension that creates an array of valid slabs according to our criteria
-        valid_slabs = [Slab(slab, bulk.CONTCAR, index, MIN_SLAB, MIN_VAC) for slab in valid_slabs]
+def findSlabs(bulk, index: tuple, MIN_SLAB: int, MIN_VAC: int):
+         
+        slabgen = SlabGenerator(bulk.structure, index, MIN_SLAB, MIN_VAC, primitive=False, in_unit_planes=True, reorient_lattice=True) #this will find all unique surfaces of the structure within the specified miller plane
+        slabs: list = slabgen.get_slabs()
+        valid_slabs: list = [slab for slab in slabs if (not slab.is_polar() and slab.is_symmetric())] #list comprehension that creates an array of valid slabs according to our criteria
+        valid_slabs: list = [Slab(slab, bulk.CONTCAR, index, MIN_SLAB, MIN_VAC) for slab in valid_slabs]
 
         return valid_slabs
+
+
+def writeKpath(CONTCAR: str):
+    struct: Structure = Structure.from_file(CONTCAR)
+    kpath = HighSymmKpath(struct)
+    kpts = Kpoints.automatic_linemode(divisions=40,ibz=kpath)
+    kpts.write_file("KPOINTS_nsc")
+
+def makeKpoints(slab: Slab, density: list=[50,50,50], save=True): 
+    '''Writes KPOINT file to current directory
+    
+       Kpoints are assigned by length [x,y,z]/[a,b,c]
+    '''
+
+    kpts = Kpoints.automatic_density_by_lengths(slab.structure, density, force_gamma=True)
+    kpoints = kpts.as_dict()['kpoints'][0]
+    if save:
+        kpts.write_file("KPOINTS")
+
+    return f"{int(kpoints[0])}x{int(kpoints[1])}x{int(kpoints[2])}"
+
+def prepareSlabs(contcars: list[str], indices: list[tuple], min_slab: int=2, min_vac: int=2):
+    '''
+    Creates a list of Slab objects from a list of CONTCARs for quick use
+    '''
+
+    bulk_list = [Bulk(contcar) for contcar in contcars]
+    slab_list = []
+    for index in indices:
+        for bulk in bulk_list:
+            for slab in findSlabs(bulk, index, min_slab, min_vac):
+                slab_list.append(slab)
+    
+    return slab_list
+    
+
+def metaData(slab_list: list[Slab], filename: str="metadata", wKPOINTS: bool=False, mode: str='w'):
+    '''Creates CSV with slab attributes
+    
+       Useful for checking possible errors
+    '''
+
+    with open(f"{filename}.csv", mode) as meta:
+        meta.write("Formula,index,a,b,c,kpoints")
+        meta.write('\n')
+
+        for slab in slab_list:
+
+            kpoints = None
+            if wKPOINTS:
+                kpoints = makeKpoints(slab)
+            
+            meta.write(f"{slab.formula},{slab.plane},{slab.a:10.5f},{slab.b:10.5f},{slab.c:10.5f},{(kpoints or '')}")
+            meta.write('\n')
+
+
+
+
+
+
+    
