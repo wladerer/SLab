f36ac6deba4ca3a96414c717ecf79243d0a2175b
William <wtladerer@gmail.com>
2022-09-14 18:50:35 -0700

    way too much

--- core/example.py
+++ core/example.py
@@ -1,5 +1,9 @@
-import bulk2slab as b2s
+from bulk2slab import *
 
-CONTCAR = "/home/wladerer/github/SLab/test/BULK_CONTCARS/Bi2Se3_CONTCAR.vasp"
-Bi2Se3_bulk = b2s.Bulk(CONTCAR)
-slabs = b2s.find_slabs(Bi2Se3_bulk, (1,0,0), 2, 2)
+Bi2Se3_contcar = "/home/wladerer/github/SLab/test/BULK_CONTCARS/Bi2Se3_CONTCAR.vasp"
+Bi2Te3_contcar = "/home/wladerer/github/SLab/test/BULK_CONTCARS/Bi2Te3_CONTCAR.vasp"
+Sb2Se3_contcar = "/home/wladerer/github/SLab/test/BULK_CONTCARS/Sb2Se3_CONTCAR.vasp"
+Sb2Te3_contcar = "/home/wladerer/github/SLab/test/BULK_CONTCARS/Sb2Te3_CONTCAR.vasp"
+contcars = [Bi2Se3_contcar, Bi2Te3_contcar, Sb2Se3_contcar, Sb2Te3_contcar]
+
+sanityCheck(contcars, [(1,0,0), (0,1,0), (0,0,1), (1,1,1)])
