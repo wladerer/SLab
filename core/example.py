045120091ed0229e49e8fe59f777d07b673982f5
William <wtladerer@gmail.com>
2022-09-30 23:51:10 -0700

    new files

--- core/example.py
+++ core/example.py
@@ -1,24 +1,19 @@
 from bulk2slab import *
 from Potcar import writePOTCAR
-import os
 
-Bi2Se3_contcar = "/home/wladerer/github/SLab/test/BULK_CONTCARS/Bi2Se3_CONTCAR.vasp"
-Bi2Te3_contcar = "/home/wladerer/github/SLab/test/BULK_CONTCARS/Bi2Te3_CONTCAR.vasp"
-Sb2Se3_contcar = "/home/wladerer/github/SLab/test/BULK_CONTCARS/Sb2Se3_CONTCAR.vasp"
-Sb2Te3_contcar = "/home/wladerer/github/SLab/test/BULK_CONTCARS/Sb2Te3_CONTCAR.vasp"
+Bi2Se3_contcar = "/SLab/test/BULK_CONTCARS/Bi2Se3_CONTCAR.vasp"
+Bi2Te3_contcar = "/SLab/test/BULK_CONTCARS/Bi2Te3_CONTCAR.vasp"
+Sb2Se3_contcar = "/SLab/test/BULK_CONTCARS/Sb2Se3_CONTCAR.vasp"
+Sb2Te3_contcar = "/SLab/test/BULK_CONTCARS/Sb2Te3_CONTCAR.vasp"
 contcars = [Bi2Se3_contcar, Bi2Te3_contcar, Sb2Te3_contcar]
 
+#Write POTCARs
 for contcar in contcars:
     writePOTCAR(contcar, "/app/vaspapp/Potentials/PBE")
 
-sb_slabs = prepareSlabs([Sb2Se3_contcar], [(1,1,1), (1,0,0), (0,1,0), (0,0,1)], 3, 4)
-all_others = prepareSlabs(contcars, [(1,1,1), (1,0,0), (0,1,0), (0,0,1)], 2, 2)
+slabs = prepareSlabs(contcars, [(1,1,1), (1,0,0), (0,1,0), (0,0,1)], 2, 2)
 
-os.chdir("/home/wladerer/github/SLab/filedump")
-
-for slab in sb_slabs:
-    all_others.append(slab)
-
-for slab in all_others:
+#Write POSCARs and KPOINTS
+for slab in slabs:
     slab.saveSlab(freeze=True)
     writeKpoints(slab)
