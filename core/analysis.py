fd7c6ce71e6b9a2a62f8c9daa7d08fc647348656
William Laderer <wtladerer@gmail.com>
2022-10-03 17:38:30 -0700

    moving stuff around; making analysis better

--- core/analysis.py
+++ core/analysis.py
@@ -1,9 +1,25 @@
 from pymatgen.io.vasp.outputs import Vasprun
+import os
+
+def files_in_dir(dir):
+
+    for filename in os.listdir(dir):
+        f = os.path.join(dir, filename)
 
 
-def isConverged(xml):
-    print(Vasprun(xml).converged)
+def isConverged(dir):
+    return Vasprun(dir).converged
 
-xml = "/home/wladerer/github/SLab/outdir/Bi2Se3_001_vasprun.xml"
+dir = "/home/wladerer/github/SLab/outdir/Bi2Se3/001/vasprun.xml"
 
-isConverged(xml)
+systems = ("Bi2Se3", "Bi2Te3", "Sb2Se3", "Sb2Te3")
+planes = ("001", "010", "100", "111")
+dir_prefix = "/home/wladerer/github/SLab/outdir"
+
+not_converged = []
+for system, plane in zip(systems,planes):
+    xml = f"{dir_prefix}/{system}/{plane}/vasprun.xml"
+    if not isConverged(xml):
+        not_converged.append(f"{system}_{plane}")
+
+print(not_converged)
