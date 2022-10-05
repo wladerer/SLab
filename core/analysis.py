168872b0d17b71173eea0f388480b1253e5c678d
William Laderer <wtladerer@gmail.com>
2022-10-04 20:43:43 -0700

    more analytical utilities

--- core/analysis.py
+++ core/analysis.py
@@ -7,19 +7,45 @@ def files_in_dir(dir):
         f = os.path.join(dir, filename)
 
 
-def isConverged(dir):
-    return Vasprun(dir).converged
+def isConverged(xml):
+    return Vasprun(xml).converged
 
-dir = "/home/wladerer/github/SLab/outdir/Bi2Se3/001/vasprun.xml"
+def getStatistics(xml):
+    data = Vasprun(xml)
+    final_energy = data.final_energy
+    band_gap, cbm, vbm, is_direct = data.eigenvalue_band_properties
+    hubbard_U = data.hubbards
+    run_type = data.run_type
+    stat_string = f"{final_energy},{band_gap},{cbm},{vbm},{is_direct},{(hubbard_U or '')},{run_type}\n"
+
+    return stat_string
+
+
+def toCSV(rows):
+
+    header = "Name,Final Energy,Band Gap,Valence band maximum,Conduction Band minimum,Direct BG,Hubbard U,Run Type"
+
+    with open("slab_data.csv", 'w') as file:
+
+        file.writelines(header + '\n')
+        file.writelines(rows)
 
 systems = ("Bi2Se3", "Bi2Te3", "Sb2Se3", "Sb2Te3")
 planes = ("001", "010", "100", "111")
 dir_prefix = "/home/wladerer/github/SLab/outdir"
-
 not_converged = []
-for system, plane in zip(systems,planes):
-    xml = f"{dir_prefix}/{system}/{plane}/vasprun.xml"
-    if not isConverged(xml):
-        not_converged.append(f"{system}_{plane}")
+rows = []
+for system in systems:
+    for plane in planes:
+        xml = f"{dir_prefix}/{system}/{plane}/vasprun.xml"
+        name = f"{system}_{plane}"
+        if not isConverged(xml):
+            not_converged.append(f"{name}")
 
-print(not_converged)
+        row = getStatistics(xml)
+        rows.append(f"{name},{row}")
+
+
+
+
+
--- core/example.py
+++ core/example.py
@@ -1,19 +1,18 @@
 from bulk2slab import *
 from Potcar import writePOTCAR
 
-Bi2Se3_contcar = "/SLab/test/BULK_CONTCARS/Bi2Se3_CONTCAR.vasp"
-Bi2Te3_contcar = "/SLab/test/BULK_CONTCARS/Bi2Te3_CONTCAR.vasp"
-Sb2Se3_contcar = "/SLab/test/BULK_CONTCARS/Sb2Se3_CONTCAR.vasp"
-Sb2Te3_contcar = "/SLab/test/BULK_CONTCARS/Sb2Te3_CONTCAR.vasp"
+Bi2Se3_contcar = "/home/wladerer/github/SLab/test/BULK_CONTCARS/Bi2Se3_CONTCAR.vasp"
+Bi2Te3_contcar = "/home/wladerer/github/SLab/test/BULK_CONTCARS/Bi2Te3_CONTCAR.vasp"
+Sb2Se3_contcar = "/home/wladerer/github/SLab/test/BULK_CONTCARS/Sb2Se3_CONTCAR.vasp"
+Sb2Te3_contcar = "/home/wladerer/github/SLab/test/BULK_CONTCARS/Sb2Te3_CONTCAR.vasp"
 contcars = [Bi2Se3_contcar, Bi2Te3_contcar, Sb2Te3_contcar]
 
 #Write POTCARs
-for contcar in contcars:
-    writePOTCAR(contcar, "/app/vaspapp/Potentials/PBE")
+# for contcar in contcars:
+#     writePOTCAR(contcar, "/app/vaspapp/Potentials/PBE")
 
 slabs = prepareSlabs(contcars, [(1,1,1), (1,0,0), (0,1,0), (0,0,1)], 2, 2)
+sb2se3_slabs = prepareSlabs([Sb2Se3_contcar], [(1,1,1), (1,0,0), (0,1,0), (0,0,1)], 3, 3)
+all_slabs = slabs + sb2se3_slabs
 
-#Write POSCARs and KPOINTS
-for slab in slabs:
-    slab.saveSlab(freeze=True)
-    writeKpoints(slab)
+metaData(all_slabs, "meta_data", wKPOINTS=True)
